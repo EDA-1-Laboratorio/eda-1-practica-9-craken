e0ba63cf6ff076571221de5fadcb682c69cd9dea
CarlosSantiago07 <carlos.chessa45@gmail.com>
2026-04-17 08:34:48 -0600

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,13 @@
+#variables numericas
+entero=0
+flotante=3.14
+booleano=True
+
+#verificar tipos
+print(type(entero))
+print(type(flotante))
+print(type(booleano))
+
+#constante (convencion)
+GRAVEDAD=9.81
+print("La gravedad es:", GRAVEDAD)
