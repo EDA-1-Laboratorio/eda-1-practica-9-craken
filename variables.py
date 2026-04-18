4d92908e78f658feb671b938f61ec7819a03d074
CarlosSantiago07 <carlos.chessa45@gmail.com>
2026-04-18 17:17:07 -0600

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,19 +1,20 @@
-#Variables numericas
-entero=0
-flotante=3.14
-booleano=True
+#Variables numéricas
+entero= 10
+flotante= 3.14
+booleano= True
 
 #Verificar tipos
 print(type(entero))
 print(type(flotante))
 print(type(booleano))
 
-#Constante (convencion)
-GRAVEDAD=9.81
+#Constante (convención)
+GRAVEDAD= 9.81
 print("La gravedad es:", GRAVEDAD)
 
 #Mi turno
 nombre= "Santiago Uribe Carlos Yael"
 semestre= "2026-2"
-print("Mi nombre es:", nombre)
-print("Semestre actual:", semestre)
+print("Mi nombre es:", nombre, type(nombre))
+print("Semestre actual:", semestre, type(semestre))
+
